68f92c3142d18d0d0be03558d6d4fad30f445fb0
MrSaints <ian@fyianlai.com>
2014-11-02 06:48:37 +0000

    Working on a solution (added Faulhaber's formula for starters; pyramid numbers)

---
+++ Elixir/lib/problem_487.ex
@@ -0,0 +1,10 @@
+defmodule Problem_487 do
+    # Faulhaber's formula
+    def f_pyramid(n) do
+        (2 * (Problem_25.pow(n, 3)) + (3 * (n * n)) + n) / 6
+    end
+
+    def answer do
+        
+    end
+end
