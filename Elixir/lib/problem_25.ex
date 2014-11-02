162f95f3e109123c3f5223d38aa6bb308acef9cc
MrSaints <ian@fyianlai.com>
2014-11-02 06:01:53 +0000

    Fixed indentation

--- Elixir/lib/problem_25.ex
+++ Elixir/lib/problem_25.ex
@@ -1,20 +1,20 @@
 # O(N ^ N)
 defmodule Problem_25 do
-	def pow(_, y, acc, i) when i === y do
-		acc
-	end
+    def pow(_, y, acc, i) when i === y do
+        acc
+    end
 
-	def pow(x, y, acc, i) do
-		pow x, y, acc * x, i + 1
-	end
+    def pow(x, y, acc, i) do
+        pow x, y, acc * x, i + 1
+    end
 
-	def pow(x, y) do
-		pow x, y, x, 1
-	end
+    def pow(x, y) do
+        pow x, y, x, 1
+    end
 
-	def answer do
-		Enum.reduce(1..1_000, &(
-			pow(&1, &1) + &2
-		)) |> Integer.to_string|> String.slice(-10, 10) |> String.to_integer
-	end
+    def answer do
+        Enum.reduce(1..1_000, &(
+            pow(&1, &1) + &2
+        )) |> Integer.to_string|> String.slice(-10, 10) |> String.to_integer
+    end
 end
